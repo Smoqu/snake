33509c850e8ffaac93a12db1a622dbbe68c1429d
Smoqu <cedric.mas.83@gmail.com>
2021-01-30 15:01:22 +0100

    build

--- out/build.js
+++ out/build.js
@@ -1,4 +1,44 @@
 "use strict";
+/// <reference path="../TSDef/p5.global-mode.d.ts" />
+let snake;
+const size = 900;
+let apple;
+function setup() {
+    createCanvas(size, size);
+    background(0);
+    frameRate(10);
+    snake = new Snake();
+    apple = new Apple();
+}
+function collision(s, a) {
+    if (s.x + s.scale / 2 <= a.x - a.scale / 2 &&
+        s.x - s.scale / 2 >= a.x + a.scale / 2 &&
+        s.y + s.scale / 2 <= a.y - a.scale / 2 &&
+        s.y - s.scale / 2 >= a.y + a.scale / 2)
+        console.log("eaten");
+}
+function draw() {
+    background(0);
+    snake.show();
+    snake.update();
+    apple.show();
+    collision(snake, apple);
+}
+function keyPressed() {
+    snake.move(keyCode);
+}
+class Apple {
+    constructor() {
+        this.scale = Math.round(size / 75);
+        this.x = Math.round(random(0, width - this.scale / 2));
+        this.y = Math.round(random(0, height - this.scale / 2));
+    }
+    show() {
+        rectMode(CENTER);
+        fill(255, 0, 0);
+        rect(this.x, this.y, this.scale, this.scale);
+    }
+}
 class Snake {
     constructor() {
         this.trail = [];
