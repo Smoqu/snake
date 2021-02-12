08128c829373de1f8d878ad49ec8a5949ebe452c
Smoqu <36056008+Smoqu@users.noreply.github.com>
2021-02-12 19:09:40 +0100

    Yes

--- src/classes/snake.ts
+++ src/classes/snake.ts
@@ -1,16 +1,43 @@
 class Snake {
   x: number;
   y: number;
-
+  direction: string;
   scale: number;
 
   constructor(x: number, y: number) {
     this.x = x;
     this.y = y;
     this.scale = floor(size / 100);
+    this.direction = "UP";
   }
 
   show() {
     rect(this.x, this.y, this.scale, this.scale);
+  }
+
+  //   update() {
+  //       switch(this.direction) {
+  //           case "UP":
+  //               this.x
+  //       }
+  //   }
+
+  move(keyCode: number) {
+    switch (keyCode) {
+      case 39:
+        this.direction = "RIGHT";
+        break;
+      case 37:
+        this.direction = "LEFT";
+        break;
+      case 38:
+        this.direction = "TOP";
+        break;
+      case 40:
+        this.direction = "BOTTOM";
+        break;
+      default:
+        break;
+    }
   }
 }
